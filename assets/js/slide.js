90f08b1f7c57ba46830a65076e80b47d3d9978e8
kototani <torayuki55_kainue@icloud.com>
2024-12-01 00:17:27 +0900

    Add files via upload

--- assets/js/slide.js
+++ assets/js/slide.js
@@ -1,5 +1,5 @@
 const h1 = document.querySelector('h1');
-      
+
 console.log(h1);
 
 const slide = document.getElementById('slide');
@@ -10,17 +10,20 @@ const lists = document.querySelectorAll('.list');
 const totalSlides = lists.length;
 let count = 0;
 let autoPlayInterval;
+
 function updateListBackground() {
   for (let i = 0; i < lists.length; i++) {
     lists[i].style.backgroundColor = i === count % totalSlides ? '#000' : '#fff';
   }
 }
+
 function nextClick() {
   slide.classList.remove(`slide${count % totalSlides + 1}`);
   count++;
   slide.classList.add(`slide${count % totalSlides + 1}`);
   updateListBackground();
 }
+
 function prevClick() {
   slide.classList.remove(`slide${count % totalSlides + 1}`);
   count--;
@@ -28,13 +31,7 @@ function prevClick() {
   slide.classList.add(`slide${count % totalSlides + 1}`);
   updateListBackground();
 }
-function startAutoPlay() {
-  autoPlayInterval = setInterval(nextClick, 3000);
-}
-function resetAutoPlayInterval() {
-  clearInterval(autoPlayInterval);
-  startAutoPlay();
-}
+
 next.addEventListener('click', () => {
   nextClick();
   resetAutoPlayInterval();
@@ -43,14 +40,5 @@ prev.addEventListener('click', () => {
   prevClick();
   resetAutoPlayInterval();
 });
-indicator.addEventListener('click', (event) => {
-  if (event.target.classList.contains('list')) {
-    const index = Array.from(lists).indexOf(event.target);
-    slide.classList.remove(`slide${count % totalSlides + 1}`);
-    count = index;
-    slide.classList.add(`slide${count % totalSlides + 1}`);
-    updateListBackground();
-    resetAutoPlayInterval();
-  }
-});
+
 startAutoPlay();
